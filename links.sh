9da8575d4f2d27a924f14d1a83c1a3752a06f2e1
Simon Doppler <dopsi.dev@gmail.com>
2016-01-01 11:29:35 +0100

    Fix errors if already existing symlinks

--- links.sh
+++ links.sh
@@ -3,13 +3,13 @@
 # $1 is the binaries directory
 # $2 is the symlinks directory
 
-if [ -z "$2/$1" ] ; then exit 1 ; fi
-if [ -z "$2/$2" ] ; then exit 1 ; fi
+if [ -f "$2/$1" ] ; then exit 1 ; fi
+if [ -f "$2/$2" ] ; then exit 1 ; fi
 
-ln -s "$1/enlightenment_start" "$2/E17"
-ln -s "$1/startfluxbox" "$2/Fluxbox"
-ln -s "$1/gnome-session" "$2/GNOME"
-ln -s "$1/icewm-session" "$2/ICEWM"
-ln -s "$1/pekwm" "$2/PekWM"
-ln -s "$1/startxfce4" "$2/XFCE4"
-ln -s "$1/openbox-session" "$2/openbox"
+ln -sf "$1/enlightenment_start" "$2/E17"
+ln -sf "$1/startfluxbox" "$2/Fluxbox"
+ln -sf "$1/gnome-session" "$2/GNOME"
+ln -sf "$1/icewm-session" "$2/ICEWM"
+ln -sf "$1/pekwm" "$2/PekWM"
+ln -sf "$1/startxfce4" "$2/XFCE4"
+ln -sf "$1/openbox-session" "$2/openbox"
