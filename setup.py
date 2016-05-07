45c26563c5a052436bbd6d8c5c8c6cee88f5aa4b
Simon Doppler <dopsi.dev@gmail.com>
2016-05-07 16:23:14 +0200

    Change informations

--- setup.py
+++ setup.py
@@ -9,29 +9,22 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "flairstats",
+    name = "tdm",
     version = "2.0.0",
     author = "Simon Doppler",
-    author_email = "dopsi.dev@gmail.com",
-    description = ("Get subreddit statistics"),
+    author_email = "dopsi@member.fsf.org",
+    description = ("Display manager-like console program"),
     license = "MIT",
-    keywords = "reddit statistics",
-    url = "http://github.com/dopsi/flairstats",
-    packages=['flairstats', 'flairstats.tools'],
+    keywords = "console display manager",
+    url = "http://github.com/dopsi/tdm",
+    packages=['tdm'],
     data_files=[
-        ('share/doc/flairstats/config', ['config/config.json']),
-        ('lib/systemd/system', ['systemd/system/flairstats-fetchbot@.service', 'systemd/system/flairstats-fetchbot@.timer']),
-        ('lib/systemd/system', ['systemd/system/flairstats-statsbot@.service', 'systemd/system/flairstats-statsbot@.timer'])
+        ('share/man/man1', ['ressources/tdm.1']),
     ],
     long_description=read('README.md'),
     classifiers=[
-        "Development Status :: 2- Pre-Alpha",
-        "Topic :: Text Processing :: Markup :: HTML",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
     ],
-    install_dependencies=[
-        'htmlgenerator',
-        'praw',
-        'pygal'
-    ]
 )
