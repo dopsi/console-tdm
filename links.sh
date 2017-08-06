4ae4ce44bd68c1517ee21b8b91c6824c7e0c5156
DuncanvR <DuncanvR@users.noreply.github.com>
2017-08-06 12:28:04 +0200

    Add symlink for xmonad (#12)
    * Sort symlinks
    
    * Add symlink for xmonad

--- links.sh
+++ links.sh
@@ -7,9 +7,10 @@ if [ -f "$2/$1" ] ; then exit 1 ; fi
 if [ -f "$2/$2" ] ; then exit 1 ; fi
 
 ln -sf "$1/enlightenment_start" "$2/E17"
-ln -sf "$1/startfluxbox" "$2/Fluxbox"
 ln -sf "$1/gnome-session" "$2/GNOME"
 ln -sf "$1/icewm-session" "$2/ICEWM"
-ln -sf "$1/pekwm" "$2/PekWM"
-ln -sf "$1/startxfce4" "$2/XFCE4"
 ln -sf "$1/openbox-session" "$2/openbox"
+ln -sf "$1/pekwm" "$2/PekWM"
+ln -sf "$1/startfluxbox" "$2/Fluxbox"
+ln -sf "$1/startxfce4" "$2/XFCE4"
+ln -sf "$1/xmonad" "$2/xmonad"
